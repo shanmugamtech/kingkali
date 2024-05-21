345923a95d5aeb995fd5e41cb39546ef0b8e819d
Darasan <107739312+shanmugamtech@users.noreply.github.com>
2024-05-21 22:04:16 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+const usernameId = document.getElementById('usernameId');
+const passwordId = document.getElementById('passwordId');
+
+function login() {
+    if (usernameId === "admin" && passwordId === "Admin@123") {} else {
+        alert('UserName Incorrect...Password Incorrect')
+    }
+}
